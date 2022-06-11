86210905a71b1bad5bb7f171e54fe0c037d640ed
Mehedi Hasan <mehedi609@gmail.com>
2022-06-12 03:19:33 +0600

    Conflict Airbnb with prettier

--- backend/src/controller/user.controller.ts
+++ backend/src/controller/user.controller.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
-import { autoInjectable } from 'tsyringe';
-import expressAsyncHandler from 'express-async-handler';
 import bcrypt from 'bcryptjs';
+import expressAsyncHandler from 'express-async-handler';
+import { autoInjectable } from 'tsyringe';
 import { UserService } from '../services';
 import {
   sendOKResponse,
