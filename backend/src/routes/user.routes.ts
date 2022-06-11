cf374036bd2c47d038c8ebb2e17f5b041f4b663c
Mehedi Hasan <mehedi609@gmail.com>
2022-06-12 01:59:16 +0600

    Implement global error handling

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -3,7 +3,6 @@ import { userController } from '../controller/user.controller';
 
 const router = Router();
 
-router.get('/', userController.all);
 router.post('/register', userController.register);
 router.post('/activate', userController.activateAccount);
 
