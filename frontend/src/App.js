a36123e59fc8a527d76f32d55bb34f9c8846228e
Mehedi Hasan <mehedi609@gmail.com>
2022-06-17 12:14:26 +0600

    Implemented react-router-dom and create some pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,16 @@
+import { Routes, Route } from "react-router-dom";
+import { Home, Login, Profile } from "./pages";
+
 function App() {
-  return <div>welcome to frontend!!</div>;
+  return (
+    <>
+      <Routes>
+        <Route path={`/`} element={<Home />} />
+        <Route path={`/login`} element={<Login />} />
+        <Route path={`/profile`} element={<Profile />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import "./styles/icons/icons.css";
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </BrowserRouter>,
   document.getElementById("root")
 );
